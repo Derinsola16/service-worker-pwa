3d087c7b015497fedb22315219dd39d13b98bfde
derinsola16 <aderinsolaodusany@gmail.com>
2021-02-03 19:33:20 +0100

    fixed another typo

--- service-worker.js
+++ service-worker.js
@@ -13,7 +13,7 @@ let cacheFiles = [
 
 self.addEventListener('install', (e) => {
   console.log('[Service Worker] Install');
-  e.waitUntill(
+  e.waitUntil(
     caches.open(cacheName).then((cache) => {
       console.log('[Service Worker] Caching all the files');
       return cache.addAll(cacheFiles)
